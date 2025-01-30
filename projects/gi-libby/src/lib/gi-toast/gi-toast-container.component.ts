d42a126754d6324aadeaca252ebe226cc50b2f32
iamgreenintro <iamgreenintro@outlook.com>
2025-01-31 00:48:47 +0100

    refactor for readability

--- projects/gi-libby/src/lib/gi-toast/gi-toast-container.component.ts
+++ projects/gi-libby/src/lib/gi-toast/gi-toast-container.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  booleanAttribute,
+  Component,
+  EventEmitter,
+  Input,
+  numberAttribute,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { ToastItem } from './types';
 import { GiToastService } from './gi-toast.service';
 
@@ -12,12 +20,16 @@ import { GiToastService } from './gi-toast.service';
 export class GiToastContainerComponent implements OnInit {
   // Input properties (and if applicable their (initial) default values):
   @Input() toastItems: ToastItem[] = [];
-  @Input() maxToastItems: number = 4;
-  @Input() toastDurationInMs: number = 5000;
+  @Input({ transform: numberAttribute }) maxToastItems: number = 4;
+  @Input({ transform: numberAttribute }) toastDurationInMs: number = 5000;
+  @Input({ transform: booleanAttribute }) animate: boolean = false;
+  @Input() animationDirection: 'right' | 'left' | 'top' | 'bottom' = 'left';
 
   // Output EventEmitters:
   @Output('close') closeEvent = new EventEmitter();
   @Output('toastListChange') toastListChangeEvent = new EventEmitter();
+
+  private _toastCount: number = 0;
 
   constructor(private toastService: GiToastService) {}
 
@@ -28,14 +40,25 @@ export class GiToastContainerComponent implements OnInit {
   }
 
   public close(toastItem: ToastItem): void {
-    if (toastItem.closeRef !== undefined) {
-      window.clearTimeout(toastItem.closeRef);
-    }
+    this._removeTimeoutReferenceIfSet(toastItem);
     this.toastService.removeToast(toastItem);
     this.closeEvent.emit();
   }
 
   private _onToastListChange(toastList: ToastItem[]): void {
+    // Check if added or removed by comparing previous length.
+    if (this._toastCount < toastList.length) {
+      this._onToastAdd(toastList);
+      this.toastListChangeEvent.emit('added!');
+    } else {
+      this.toastListChangeEvent.emit('closed!');
+    }
+
+    // Finally - update the previous toast count to the current one.
+    this._toastCount = toastList.length;
+  }
+
+  private _onToastAdd(toastList: ToastItem[]): void {
     this._closeFirstIfExceededMaxAmount(toastList);
 
     const toast: ToastItem = toastList[toastList.length - 1];
@@ -46,8 +69,6 @@ export class GiToastContainerComponent implements OnInit {
         }, this.toastDurationInMs);
       }
     }
-
-    this.toastListChangeEvent.emit('changed!');
   }
 
   private _closeFirstIfExceededMaxAmount(toastList: ToastItem[]): void {
